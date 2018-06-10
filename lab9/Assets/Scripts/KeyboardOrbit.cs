c646f7946cb9371f366c46c41bde0fecacfab026
patrykpp <patryk583@vp.pl>
2018-06-10 19:52:54 +0200

    lab9

---
+++ lab9/Assets/Scripts/KeyboardOrbit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class KeyboardOrbit : MonoBehaviour
+{
+    public int Speed = 5;
+
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Vertical") * Speed;
+        float zAxisValue = -Input.GetAxis("Horizontal") * Speed;
+
+        transform.position = new Vector3(transform.position.x + xAxisValue, transform.position.y, transform.position.z + zAxisValue);
+    }
+}
